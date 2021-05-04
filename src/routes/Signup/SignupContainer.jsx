a64cec3773103d69c3306c65e209a397c411f0ef
hyewonji <60416187+hyewonji@users.noreply.github.com>
2021-05-04 18:45:30 +0900

    feat : signup form 닉네임, 비밀번호 일치 입력칸 생성

--- src/routes/Signup/SignupContainer.jsx
+++ src/routes/Signup/SignupContainer.jsx
@@ -1,44 +1,101 @@
-import React from 'react';
-import styled from 'styled-components'
-import { useForm } from 'react-hook-form';
+import React from "react";
+import styled from 'styled-components';
+import { useForm } from "react-hook-form";
 
-function SignupContainer () {
 
-  const { handleSubmit, register, errors } = useForm();
+const SignupFrom = styled.form`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`
 
-  const onSubmit = (values) => {
-      const { email, nickname, password, confirmPassword } = values;
+const Label = styled.label``
+const Input = styled.input``
+
+export default function SignupContainer() {
+  const { register, handleSubmit, formState: { errors }, reset } = useForm();
+
+  const checkPassword = (password, confirmPassword) => {
+    if (password === confirmPassword){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  const onSubmit = data => {
+    const { email, nickname, password, confirmPassword} = data;
+    const Password = checkPassword(password,confirmPassword)
+      
+    if (Password){
+      console.log(true)
+    } else{
+      console.log(false)
+    }
+    
+    reset();
   };
 
-  const SignupForm = styled.form``
-  const Input = styled.input``
-  const SubmitButton = styled.button``
-
   return (
-    <SignupForm onSubmit={handleSubmit(onSubmit)}>
+    <SignupFrom onSubmit={handleSubmit(onSubmit)}>
+      <Label htmlFor="email">email</Label>
       <Input
-        name="email"
-        placeholder="이메일"
-        {...register("message", {
-            required: "Required",
-            pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: "invalid email address",
-              }
-          })}
-      />
-      <Input
-        name="password"
-        placeholder="패스워드"
-        type="password"
-        {...register("message", {
-            required: "Required",
-            validate: value => value !== "admin" || "Nice try!",
+        id="email"
+        {...register("email", {
+          required: "required",
+          pattern: {
+            value: /\S+@\S+.\S+/,
+            message: "Entered value does not match email format"
+          }
         })}
+        type="email"
       />
-      <SubmitButton type="submit">회원가입</SubmitButton>
-    </SignupForm>
+      {errors.email && <span role="alert">{errors.email.message}</span>}
+      <Label htmlFor="nickname">nickname</Label>
+      <Input
+        id="nickname"
+        {...register("nickname", {
+          required: "required",
+          minLength: {
+            value: 2,
+            message: "min length is 2"
+          },
+          maxLength: {
+            value: 8,
+            message: "max length is 8"
+          }
+        })}
+        type="text"
+      />
+      {errors.nickname && <span role="alert">{errors.nickname.message}</span>}
+      <Label htmlFor="password">password</Label>
+      <Input
+        id="password"
+        {...register("password", {
+          required: "required",
+          minLength: {
+            value: 8,
+            message: "min length is 8"
+          },
+          maxLength: {
+            value: 20,
+            message: "max length is 20"
+          }
+        })}
+        type="password"
+      />
+      {errors.password && <span role="alert">{errors.password.message}</span>}
+      <Label htmlFor="confirmPassword">confirmPassword</Label>
+      <Input
+        id="confirmPassword"
+        {...register("confirmPassword", {
+          required: "required",
+        })}
+        type="password"
+      />
+      {errors.confirmPassword && <span role="alert">{errors.confirmPassword.message}</span>}
+      <button type="submit">SUBMIT</button>
+    </SignupFrom>
   );
 }
-
-export default SignupContainer;
